204b9575be28d87f20b5f0a7b0e84b3dcba88cbf
Joe Betz <jpbetz@google.com>
2021-05-20 09:44:08 -0700

    fix CEL support

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
+TODO:
+
+- [ ] add informer to fetch crds and register them
+- [ ] add details on how to register with apiserver
+- [ ] add end-to-end examples
+
+How to run:
+
 go build . && ./runner-webhook webhook \
   --tls-cert-file $HOME/projects/k8s/src/k8s.io/kubernetes/hack/testdata/tls.crt \
   --tls-private-key-file $HOME/projects/k8s/src/k8s.io/kubernetes/hack/testdata/tls.key \
   --port 8084 \
   -v 4
+
+How to test directly:
 
 curl -H "Content-Type: application/json" -kv https://localhost:8084/validate --data @example/crontab/admissionreview.json | jq .
--- validators/cel.go
+++ validators/cel.go
@@ -3,10 +3,13 @@ package validators
 import (
 	"fmt"
 
+	"github.com/google/cel-go/common/types"
+	celext "github.com/google/cel-go/ext"
 	"github.com/google/cel-go/cel"
 	"github.com/google/cel-go/checker/decls"
 )
 
+// https://github.com/tektoncd/triggers/blob/7c165fc7f5f54d6925672ff5061957b5e224135d/pkg/interceptors/cel/cel.go#L71
 type CelValidator struct {
 }
 
@@ -17,12 +20,21 @@ func NewCelValidator() *CelValidator {
 
 func (v *CelValidator) Validate(celCode string, obj interface{}) error {
 	env, err := cel.NewEnv(
+		celext.Strings(),
+		celext.Encoders(),
 		cel.Declarations(
-			decls.NewVar("field", decls.Dyn))) // TODO: map to actual schema type?
+			// TODO: for maps, explode out all the fields so we can do "replicas < 5" instead of "field.replicas < 5"
+			// TODO: for primitives, match their exact type? Or just use Dyn.
+			// The more type information provided here the more CEL errors are caught at development time.
+			decls.NewVar("field", decls.NewMapType(decls.String, decls.Dyn)))) // TODO: map to actual schema type?
 	if err != nil {
 		return fmt.Errorf("error initializing CEL environment: %w", err)
 	}
 	ast, issues := env.Compile(celCode)
+	if issues != nil && issues.Err() != nil {
+		return fmt.Errorf("CEL compile error: %w", issues.Err())
+	}
+	ast, issues = env.Check(ast)
 	if issues != nil && issues.Err() != nil {
 		return fmt.Errorf("CEL type-check error: %w", issues.Err())
 	}
@@ -31,16 +43,16 @@ func (v *CelValidator) Validate(celCode string, obj interface{}) error {
 	if err != nil {
 		return fmt.Errorf("CEL program construction error: %w", err)
 	}
+
 	out, _, err := prg.Eval(map[string]interface{}{
-		"field": obj,
+		"field": types.NewDynamicMap(types.DefaultTypeAdapter, obj),
 	})
 	if err != nil {
-		return fmt.Errorf("CEL program evaluation error: %w", err)
+		return fmt.Errorf("CEL program evaluation error: %w for: %#+v, cel: %s", err, obj, celCode)
 	}
-	return fmt.Errorf("cel result: %v", out.Value())
-	//if out.Value()  != true {
-	//	// TODO: Will need a much better error reported back here
-	//	return fmt.Errorf("field failed CEL validation rule")
-	//}
-	//return nil
+	if out.Value()  != true {
+		// TODO: Will need much better error reporting here
+		return fmt.Errorf("CEL format expression evaluated to false: %s", celCode)
+	}
+	return nil
 }
